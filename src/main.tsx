735b3b98d97ea7ef9a4b4b3e38ceb13011f3d1b4
Erik Ondruš <ondrus.eodesign@gmail.com>
2025-05-24 11:44:36 +0200

    ✅Fáza 8 - Refaktorovanie importov v main.tsx: Zjednodušenie importov pre App a index.css, deklaracia typov pre index css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import App from "./App";
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import { Toaster } from "sonner";
 import { BrowserRouter } from "react-router-dom";
 
